88609a8e854a8ac6390cf2222b4c05fe68b97048
Daniil <daniil-murashov@yandex.ru>
2021-08-02 14:28:57 +0300

    add apline host and create config script

---
+++ configure.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -eux
+if [[ -e /usr/sbin/sshd ]]; then
+	/usr/sbin/sshd -De
+else
+apk add --no-cache openssh
+sed -i 's|#Port 22|Port 32|g' /etc/ssh/sshd_config
+sed -i 's|#PermitRootLogin prohibit-password|PermitRootLogin yes|g' /etc/ssh/sshd_config
+apk add openssh-server 
+ssh-keygen -t rsa -f /root/id_rsa -q -P ""
+ssh-keygen -A
+echo "root:toor" | chpasswd
+mkdir ~/.ssh 
+while ! [ -e id_rsa.pub ]; do sleep 1; done 
+cp id_rsa.pub ~/.ssh/authorized_keys 
+/usr/sbin/sshd -De
+fi
