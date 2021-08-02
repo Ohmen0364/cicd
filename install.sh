d569a32c77442ac7c6a71ace4d70f0d1577eff49
Daniil <daniil-murashov@yandex.ru>
2021-08-02 21:08:01 +0300

    add install script and jenkins

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo wget https://github.com/moparisthebest/static-curl/releases/download/v7.78.0/curl-amd64 -O /usr/bin/curl
+sudo chmod +x /usr/bin/curl
+sudo curl -fsSL https://get.docker.com/ | sudo sh
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo systemctl disable apache2
+sudo usermod -aG docker $USER
