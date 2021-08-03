6bbd6bcac317d06f16627aa89b64af41f4a83fc3
Daniil <daniil-murashov@yandex.ru>
2021-08-03 13:15:33 +0300

    change code

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello, Docker! <3 1222")
+		return c.HTML(http.StatusOK, "Hello, Docker! <3333333333333")
 	})
 
 	e.GET("/ping", func(c echo.Context) error {
